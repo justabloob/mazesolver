1519ca8b304585fa8282ceec35d5039f18b0927d
justabloob <justabloob@gmail.com>
2025-04-21 19:22:45 +0200

    added maze class

---
+++ maze.py
@@ -0,0 +1,48 @@
+from cell import Cell
+import time
+import random
+
+class Maze():
+    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win):
+        self._cells = []
+        self._x1 = x1
+        self._y1 = y1
+        self._num_rows = num_rows
+        self._num_cols = num_cols
+        self._cell_size_x = cell_size_x
+        self._cell_size_y = cell_size_y
+        self._win = win
+
+        self._create_cells()
+
+    def _create_cells(self):
+        # Loop through columns (i is the column index)
+        for i in range(self._num_cols):
+            # Create a new column (which is a list)
+            column = []
+            # Loop through rows (j is the row index)
+            for j in range(self._num_rows):
+                # Create a new Cell in each position and add it to the column
+                column.append(Cell(self._win))
+                        # Add the completed column to the cells matrix
+            self._cells.append(column)
+        # After creating all cells, draw each one
+        for i in range(self._num_cols):
+            for j in range(self._num_rows):
+                self._draw_cell(i, j)
+
+    def _draw_cell(self, i, j):
+        if self._win is None:
+            return
+        x1 = self._x1 + i * self._cell_size_x
+        y1 = self._y1 + j * self._cell_size_y
+        x2 = x1 + self._cell_size_x
+        y2 = y1 + self._cell_size_y
+        self._cells[i][j].draw(x1, y1, x2, y2)
+        self._animate()
+
+    def _animate(self):
+        if self._win is None:
+            return
+        self._win.redraw()
+        time.sleep(0.05) 
